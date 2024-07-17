9ab8bc853d9bb5d98706ae92419454975b71e08e
Chan Hyuk Lee <chan5759@naver.com>
2024-07-17 14:10:17 +0900

    6th commit

--- helloworld/test.c
+++ helloworld/test.c
@@ -1,36 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 좌표 구조체 정의
 typedef struct {
-    int x;
-    int y;
+    int value; // 원래 좌표 값
+    int index; // 원래 좌표의 인덱스
 } Coordinate;
 
+// 비교 함수: qsort를 위해 사용
 int compare(const void *a, const void *b) {
-    Coordinate *cA  = (Coordinate *)a;
-    Coordinate *cB  = (Coordinate *)b;
-    if (cA->x != cB->x) {
-        return cA->x - cB->x;
-    } else {
-        return cA->y - cB->y;
-    }
+    return ((Coordinate*)a)->value - ((Coordinate*)b)->value;
 }
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    Coordinate *coordinates = (Coordinate *)malloc(n * sizeof(Coordinate));
+    Coordinate *coords = (Coordinate*)malloc(n * sizeof(Coordinate));
+    int *result = (int*)malloc(n * sizeof(int));
 
-    for (int i=0; i<n; i++) {
-        scanf("%d %d", &coordinates[i].x, &coordinates[i].y);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &coords[i].value);
+        coords[i].index = i; // 원래 인덱스를 저장
     }
 
-    qsort(coordinates, n, sizeof(Coordinate), compare);
+    // 좌표를 값 기준으로 정렬
+    qsort(coords, n, sizeof(Coordinate), compare);
 
-    for (int i=0; i<n; i++) {
-        printf("%d %d\n",coordinates[i].x, coordinates[i].y);
+    // 압축된 좌표를 계산
+    int rank = 0;
+    result[coords[0].index] = rank;
+    for (int i = 1; i < n; i++) {
+        if (coords[i].value != coords[i - 1].value) {
+            rank++;
+        }
+        result[coords[i].index] = rank;
     }
 
-    free(coordinates);
+    // 원래 순서대로 결과 출력
+    for (int i = 0; i < n; i++) {
+        printf("%d ", result[i]);
+    }
+    printf("\n");
+
+    free(coords);
+    free(result);
+
+    return 0;
 }
--- lab py/test.py
+++ lab py/test.py
@@ -1,4 +1,10 @@
-a=[1,2,3,4]
-a.append(5)
-a.reverse()
-print(a)
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+coords = list(map(int, input().split()))
+
+coords_dict = {value: index for index, value in enumerate(sorted(set(coords)))}
+compressed_coords = [coords_dict[x] for x in coords]
+
+print(' '.join(map(str, compressed_coords)))
